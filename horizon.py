5d651082ea411267817d0d99f0f97a5162ba2af6
jhoule42 <jphoule29@hotmail.com>
2020-06-14 10:08:56 -0500

    Verification d'execution

--- horizon.py
+++ horizon.py
@@ -16,7 +16,7 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
     Contact: martin.aube@cegepsherbrooke.qc.ca
 -----------------------------------------------------------------------
-# Statut : Comparer avec .f
+# Statut : Fonctionel
 -----------------------------------------------------------------------"""
 # coding=utf-8
 
